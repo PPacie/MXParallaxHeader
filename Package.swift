dbabdb21798f27417edc10d89240a0a2074bf66b
Pablo Paciello <ppacie@gmail.com>
2020-05-07 19:32:36 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 // swift-tools-version:5.1
 import PackageDescription
-
+/*
 let package = Package(
     name: "MXParallaxHeader",
     platforms: [.iOS(.v10)],
@@ -12,5 +12,25 @@ let package = Package(
         .target(
             name: "MXParallaxHeader"
         )
+    ]
+)
+*/
+
+let package = Package(
+    name: "MXParallaxHeader",
+    platforms: [.iOS(.v10),],
+    products: [
+        .library(
+            name: "MXParallaxHeader",
+            targets: ["MXParallaxHeader"]),
+    ],
+    dependencies: [
+        // no dependencies
+    ],
+    targets: [
+        .target(
+            name: "MXParallaxHeader",
+            dependencies: [],
+            path: "Sources")
     ]
 )
