0293e08fc182acf84552219e8467805e68c86462
Pablo Paciello <ppacie@gmail.com>
2020-05-07 20:00:56 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,20 +1,5 @@
 // swift-tools-version:5.1
 import PackageDescription
-/*
-let package = Package(
-    name: "MXParallaxHeader",
-    platforms: [.iOS(.v10)],
-    products: [
-        .library(name: "MXParallaxHeader",
-                 targets: ["MXParallaxHeader"])
-    ],    
-    targets: [
-        .target(
-            name: "MXParallaxHeader"
-        )
-    ]
-)
-*/
 
 let package = Package(
     name: "MXParallaxHeader",
